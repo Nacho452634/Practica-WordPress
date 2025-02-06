85ee1bf0f6ea5a94c200316544f684db9a800955
nacho <rodrigueznacho897@gmail.com>
2025-02-06 12:59:44 +0000

    header+footer

--- contacte.php
+++ contacte.php
@@ -1,43 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="img/crown_icon_245698.ico" type="image/x-icon">
-    <title>Home</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="css/styles.css">
-</head>
-<body>
-    <div class="container-fluid">
-        <div class="row">
-            <!--DIV de Nav var-->
-            <nav class="navbar navbar-expand-md navbar-dark navegacion p-3">
-                <div class="container-fluid grid gap-5">
-                    <a class="navbar-brand navegacion " href="index.html">Blogful</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0 grid gap-5">
-                            <li class="nav-item">
-                                <a class="nav-link pb-1" aria-current="page" href="index.html">Home</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link pb-1" aria-current="page" href="noticies.html">Noticies</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link active pb-1" aria-current="page" href="contacte.html">Contacte</a>
-                            </li>
-                            <li class="nav-item  mt-1">
-                                <img src="img/lupa.png" alt="">
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        </div>
-    </div>
+<?php get_header() ?> 
+
     <header id="showcase"></header>
     <div class="container-fluid custom-container">
         <div class="row">
@@ -67,49 +29,4 @@
         <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6901.181632252629!2d2.6915545289071523!3d39.601825329060716!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1297935d441c9571%3A0xa01dc49e4565742f!2sEs%20Liceu!5e0!3m2!1ses!2ses!4v1737041012572!5m2!1ses!2ses" width="1200" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
     <div class="homepage_header4-1 border-bottom border-3 col-8"></div>
-        <div class="row m-1">
-            <div class="homepage_header4 mb-5 weight col-4">
-                <p class="mb-5">Blogful</p>
-                <div class="">
-                    <p>Blogs</p>
-                    <p>Contact</p>
-                    <p>Quick Links</p>
-                </div>
-            </div>
-            <div class="homepage_header4 col-6">
-                <p class="weight">Suscribe for Newsletter</p>
-                <div class="">
-                    <p>Get our hand-picked discoveries & articles delivered right to your inbox.</p>
-                </div>
-                <div>
-                    <input class="px-2 pt-2 pb-2 email-address" type="text" placeholder="Email address">
-                    <button type="button" class="btn boton m-2 btn-grande">SUBMIT</button>
-                </div>
-                <div class="weight mt-5">
-                    <p>Follow on</p>
-                    <div class="margin-footer">
-                        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="36" height="36" viewBox="0 0 24 24">
-                            <path d="M21,23l-4.378-3.406L17,21H5c-1.105,0-2-0.895-2-2V5c0-1.105,0.895-2,2-2h14c1.105,0,2,0.895,2,2V23z M16.29,8.57	c0,0-1.23-0.95-2.68-1.06l-0.3,0.61C12.86,8.04,12.4,7.98,12,7.98c-0.41,0-0.88,0.06-1.31,0.14l-0.3-0.61	C8.87,7.66,7.71,8.57,7.71,8.57s-1.37,1.98-1.6,5.84C7.49,15.99,9.59,16,9.59,16l0.43-0.58c-0.44-0.15-0.82-0.35-1.21-0.65	l0.09-0.24c0.72,0.33,1.65,0.53,3.1,0.53s2.38-0.2,3.1-0.53l0.09,0.24c-0.39,0.3-0.77,0.5-1.21,0.65L14.41,16	c0,0,2.1-0.01,3.48-1.59C17.66,10.55,16.29,8.57,16.29,8.57z M10,13.38c-0.52,0-0.94-0.53-0.94-1.18c0-0.65,0.42-1.18,0.94-1.18	s0.94,0.53,0.94,1.18C10.94,12.85,10.52,13.38,10,13.38z M14,13.38c-0.52,0-0.94-0.53-0.94-1.18c0-0.65,0.42-1.18,0.94-1.18	s0.94,0.53,0.94,1.18C14.94,12.85,14.52,13.38,14,13.38z"></path>
-                        </svg>
-                        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="36" height="36" viewBox="0 0 30 30">
-                            <path d="M24,4H6C4.895,4,4,4.895,4,6v18c0,1.105,0.895,2,2,2h10v-9h-3v-3h3v-1.611C16,9.339,17.486,8,20.021,8 c1.214,0,1.856,0.09,2.16,0.131V11h-1.729C19.376,11,19,11.568,19,12.718V14h3.154l-0.428,3H19v9h5c1.105,0,2-0.895,2-2V6 C26,4.895,25.104,4,24,4z"></path>
-                        </svg>
-                        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="36" height="36" viewBox="0 0 24 24">
-                            <path d="M 11.941406 2 C 10.44006 2 9.0197829 2.3412832 7.7402344 2.9375 C 7.7318419 2.9350471 7.723233 2.9321205 7.7148438 2.9296875 L 7.7070312 2.9550781 C 4.3079768 4.5545168 1.9414062 8.0036548 1.9414062 12 C 1.9414062 17.511002 6.4304007 22 11.941406 22 C 17.452412 22 21.941406 17.511002 21.941406 12 C 21.941406 6.4889982 17.452412 2 11.941406 2 z M 11.941406 4 C 13.657837 4 15.156466 4.6541789 16.457031 5.5683594 C 15.82545 6.1959812 15.089919 6.7511088 14.279297 7.203125 C 13.218022 5.9232251 11.92657 4.9041032 10.568359 4.140625 C 11.013413 4.0578722 11.467449 4 11.941406 4 z M 7.9765625 5.1445312 C 9.5261871 5.727606 11.083012 6.6842895 12.335938 8.03125 C 10.255733 8.6629299 7.8727211 8.6247295 5.4804688 7.5253906 C 6.144503 6.5676373 6.9934191 5.7540149 7.9765625 5.1445312 z M 17.955078 6.8632812 C 18.812659 7.8685526 19.351381 9.0869574 19.626953 10.433594 C 18.504078 10.295389 17.358675 10.360744 16.236328 10.587891 C 16.008339 9.9709737 15.730811 9.388059 15.414062 8.8398438 C 16.357582 8.2827458 17.216233 7.6142914 17.955078 6.8632812 z M 4.4707031 9.3710938 C 7.6503928 10.893288 10.874955 10.783354 13.613281 9.71875 C 13.882417 10.169566 14.11829 10.647248 14.314453 11.154297 C 11.180821 12.379806 8.4850686 14.959562 7.4160156 18.427734 C 5.3641713 16.981851 3.9414062 14.7103 3.9414062 12 C 3.9414062 11.066479 4.1790273 10.202866 4.4707031 9.3710938 z M 17.800781 12.386719 C 18.486321 12.374364 19.15505 12.492293 19.818359 12.609375 C 19.618166 15.147299 18.276058 17.35498 16.294922 18.681641 C 16.755243 17.353288 17.110099 15.605394 16.900391 13.474609 C 16.868577 13.151845 16.819349 12.838322 16.763672 12.529297 C 16.879289 12.505865 16.993531 12.466165 17.109375 12.447266 C 17.342126 12.409292 17.572268 12.390837 17.800781 12.386719 z M 14.832031 13.134766 C 14.859562 13.312804 14.892087 13.488551 14.910156 13.671875 C 15.247834 17.102926 14.055291 19.157268 13.591797 19.822266 C 13.060483 19.935144 12.509851 20 11.941406 20 C 10.968416 20 10.062462 19.764916 9.2011719 19.449219 C 9.961496 16.505095 12.219059 14.259566 14.832031 13.134766 z"></path>
-                        </svg>
-                        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="36" height="36" viewBox="0 0 24 24">
-                            <path d="M 8 3 C 5.243 3 3 5.243 3 8 L 3 16 C 3 18.757 5.243 21 8 21 L 16 21 C 18.757 21 21 18.757 21 16 L 21 8 C 21 5.243 18.757 3 16 3 L 8 3 z M 8 5 L 16 5 C 17.654 5 19 6.346 19 8 L 19 16 C 19 17.654 17.654 19 16 19 L 8 19 C 6.346 19 5 17.654 5 16 L 5 8 C 5 6.346 6.346 5 8 5 z M 17 6 A 1 1 0 0 0 16 7 A 1 1 0 0 0 17 8 A 1 1 0 0 0 18 7 A 1 1 0 0 0 17 6 z M 12 7 C 9.243 7 7 9.243 7 12 C 7 14.757 9.243 17 12 17 C 14.757 17 17 14.757 17 12 C 17 9.243 14.757 7 12 7 z M 12 9 C 13.654 9 15 10.346 15 12 C 15 13.654 13.654 15 12 15 C 10.346 15 9 13.654 9 12 C 9 10.346 10.346 9 12 9 z"></path>
-                        </svg>
-                        <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="36" height="36" viewBox="0 0 32 32">
-                            <path d="M 1 6.75 L 1 25.25 L 9.9375 25.25 C 10.765625 25.25 16.46875 25.082031 16.46875 19.875 C 16.46875 16.621094 14.230469 15.515625 13.09375 15.1875 C 13.929688 14.792969 15.59375 13.855469 15.59375 11.34375 C 15.59375 9.945313 15.394531 6.75 9.6875 6.75 Z M 21.375 8.46875 L 21.375 10.15625 L 28.28125 10.15625 L 28.28125 8.46875 Z M 5.0625 9.90625 L 8.875 9.90625 C 9.234375 9.90625 11.65625 9.714844 11.65625 12.03125 C 11.65625 14.011719 9.90625 14.21875 9.1875 14.21875 L 5.0625 14.21875 Z M 25 11.6875 C 19.777344 11.6875 18.6875 16.222656 18.6875 18.1875 C 18.6875 24.019531 23.46875 24.6875 25 24.6875 C 29.128906 24.6875 30.300781 22.015625 30.78125 20.53125 L 27.78125 20.53125 C 27.664063 20.914063 26.789063 22.15625 25.125 22.15625 C 22.335938 22.15625 22.09375 19.976563 22.09375 19 L 30.96875 19 C 31.148438 15.625 29.671875 11.6875 25 11.6875 Z M 24.90625 14.21875 C 25.738281 14.21875 26.410156 14.460938 26.8125 14.90625 C 27.214844 15.355469 27.511719 16.011719 27.59375 16.875 L 22.09375 16.875 C 22.109375 16.636719 22.164063 16.363281 22.25 16.0625 C 22.335938 15.753906 22.46875 15.460938 22.6875 15.1875 C 22.90625 14.917969 23.210938 14.683594 23.5625 14.5 C 23.921875 14.3125 24.367188 14.21875 24.90625 14.21875 Z M 5.0625 17 L 9.46875 17 C 10.34375 17 12.40625 17.136719 12.40625 19.65625 C 12.40625 22.097656 9.808594 22.09375 9.40625 22.09375 L 5.0625 22.09375 Z"></path>
-                        </svg>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</body>
-<script src="script.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
-</html>
+<?php get_footer() ?>
---
+++ header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="img/crown_icon_245698.ico" type="image/x-icon">
+    <title>Home</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <header>
+        <div class="container-fluid">
+            <div class="row">
+            <!--DIV de Nav var-->
+            <nav class="navbar navbar-expand-md navbar-dark navegacion p-3">
+                <div class="container-fluid grid gap-5">
+                    <a class="navbar-brand navegacion " href="index.html">Blogful</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0 grid gap-5">
+                            <li class="nav-item">
+                                <a class="nav-link active pb-1" aria-current="page" href="index.html">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link pb-1" aria-current="page" href="noticies.html">Noticies</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link pb-1" aria-current="page" href="contacte.html">Contacte</a>
+                            </li>
+                            <li class="nav-item  mt-1">
+                                <img src="img/lupa.png" alt="">
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </div>
+    </header>
