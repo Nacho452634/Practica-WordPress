50a829eeb597dc67c83c9b95e3110b6be5da6a52
nacho <rodrigueznacho897@gmail.com>
2025-02-13 12:45:53 +0000

    front-page i home

---
+++ front-page.php
@@ -0,0 +1,188 @@
+<?php get_header() ?>
+
+    <div class="container-fluid custom-container">
+        
+        <!--DIV HOMEPAGE HEADER-->
+
+        <div class="row">
+            <div class="title_homepage_header col-12 col-md-6">
+                    <h2 class="mb-4 spacing">Today's picks</h2>
+                    <img class="" src="img/Image placeholder (1).png" alt="">
+            </div>
+            <div class="info_homepage_header">
+                <button type="button" class="btn btn-outline-primary boton rounded-pill m-3 btn-img">
+                    <a class="btn-img text-decoration-none" href="noticies.html">Notícies</a>
+                </button>
+            </div>
+            <div class="info_homepage_header ms-2">
+                <p class="m-1 grey">Kate Glotova | Art Director | 5 min read</p>
+                <p class="m-1 grey">March 13 ,2022</p>
+            </div>
+            <div class="info_homepage_header ms-2 mb-5 pb-3">
+                <h3 class="mt-4 anima">Anima Introduces: Hotspots Hints</h3>
+                <p class="grey2">Write the blog description right here, Write the blog description right here, Write the blog description <br>
+                right here.Write the blog description right here, Write the blog description right here, Write the blog <br>
+                description right here. Write the blog description right here, Write the blog description right here, <br>
+                Write the blog description right here.</p>
+                <a class="learn_more text-decoration-none" href="https://www.animaapp.com/" target="_blank"> Learn More </a>
+            </div>
+        </div>
+    <!--DIV Header-->
+        <div class="row">
+            <div class="info_homepage_header col-12 col-md-6 pb-5 border-bottom border-1">
+                <img src="img/Image placeholder.png" alt="" >
+                <div>
+                    <button type="button" class="btn btn-outline-primary boton rounded-pill m-3 btn-img">
+                        <a class="btn-img text-decoration-none" href="noticies.html">Notícies</a>
+                    </button>
+                </div>
+                <div>
+                    <p class="m-1 grey">Lorenzo Hamers | Product designer</p>
+                    <p class="m-1 mb-4 grey">4 min read | October 13,2022</p>
+                </div>
+                <div class="col-12 col-md-6">
+                    <a class="text-decoration-none link-dark line-height" href="noticia.html">
+                        <h3 class="line-height">Better Forms with Input Types, Auto-Complete, and Validation</h3>
+                    </a>
+                    <p class="p_header">Anima allows designers to create high-fidelity prototypes inside all design tools.</p>
+                </div>
+            </div>
+            <div class="col-12 col-md-6 pb-5 border-bottom border-1">
+                <img src="img/Image placeholder_estrellas.png" alt="">
+                <div>
+                    <button type="button" class="btn btn-outline-primary boton rounded-pill m-3 btn-img">
+                        <a class="btn-img text-decoration-none" href="noticies.html">Notícies</a>
+                    </button>
+                </div>
+            <div>
+                    <p class="m-1 grey">Mathias Reding | Game Boost | 4 min read</p>
+                    <p class="m-1 mb-4 grey">November 11,2022</p>
+                </div>
+                <div class="col-sm-6 col-md-8">
+                    <h3 class="line-height">Anima Introduces: the best Hints you should know</h3>
+                    <p class="p_header col-10">Hotspot Hints Help Guide Users Navigate Through a Prototype, Anima allows designers to create prototypes.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="homepage_header">
+                <h2 class="mb-5 ">Trending topics</h2>
+            </div>
+            <div class="row">
+                <div class="info_homepage_header col-6 pb-5">
+                    <img src="img/Image_placeholder_1.png" alt="">
+                    <div>
+                        <button type="button" class="btn btn-outline-primary boton rounded-pill m-3 btn-img">
+                            <a class="btn-img text-decoration-none" href="noticies.html">Notícies</a>
+                        </button>
+                    </div>
+                    <div>
+                        <p class="m-1 grey">Kate Glotova | Art Director | 5 min read</p>
+                        <p class="m-1 mb-4 grey">March 13 ,2022</p>
+                    </div>
+                    <div class="col-sm-6 col-md-10 mb-4">
+                        <h3 class="line-height col-10">Anima Introduces: the best Hints you should know</h3>
+                    </div>
+                    <div class="col-sm-6 col-md-8">
+                        <p class="p_header">Hotspot Hints Help Guide Users Navigate Through a Prototype, Anima allows designers to create prototypes.</p>
+                    </div>
+                </div>
+                <div class="col-6 pb-5">
+                    <img src="img/Image_placeholder_sharing.png" alt="">
+                    <div>
+                        <button type="button" class="btn btn-outline-primary boton rounded-pill m-3 btn-img">
+                            <a class="btn-img text-decoration-none" href="noticies.html">Notícies</a>
+                        </button>
+                    </div>
+                <div>
+                        <p class="m-1 grey">Kate Glotova | Art Director | 4 min read</p>
+                        <p class="m-1 mb-4 grey">September 23,2022</p>
+                    </div>
+                    <div class="col-sm-6 col-md-10 mb-4" >
+                        <h3 class="line-height">Talking now about sharing!</h3>
+                    </div>
+                    <div class="col-sm-6 col-md-7">
+                        <p class="p_header">Typography in advertising often reflects a company's brand. A brand may use typography to express its theme, personality, and message.</p>
+                    </div>
+                </div>
+                <div class="info_homepage_header col-6 pb-5">
+                    <img src="img/finestres.png" alt="">
+                    <div>
+                        <button type="button" class="btn btn-outline-primary boton rounded-pill m-3 btn-img">
+                            <a class="btn-img text-decoration-none" href="noticies.html">Notícies</a>
+                        </button>
+                    </div>
+                    <div>
+                        <p class="m-1 grey">Stephen Leonardi | Software Engineer</p>
+                        <p class="m-1 mb-4 grey">September 8 ,2022 | 3 min read</p>
+                    </div>
+                    <div class="col-sm-6 col-md-8 mb-4">
+                        <h3 class="line-height">Learn how to organize Your Anima Project with Sections</h3>
+                    </div>
+                    <div class="col-sm-6 col-md-8">
+                        <p class="p_header">Create dividers and drag screens to arrange your Anima projects.</p>
+                    </div>
+                </div>
+                <div class="col-6 pb-5">
+                    <img src="img/porta.png" alt="">
+                    <div>
+                        <button type="button" class="btn btn-outline-primary boton rounded-pill m-3 btn-img">
+                            <a class="btn-img text-decoration-none" href="noticies.html">Notícies</a>
+                        </button>
+                    </div>
+                <div>
+                        <p class="m-1 grey">Jessica Cordoba | Frontend Developer</p>
+                        <p class="m-1 mb-4 grey">4 min read | May 24,2022</p>
+                    </div>
+                    <div class="col-sm-6 col-md-7 mb-4" >
+                        <h3 class="line-height">What you need to know about code languages</h3>
+                    </div>
+                    <div class="col-sm-6 col-md-7">
+                        <p class="p_header">Rather than having to jump into strict syntax rules, Python reads like English and is simple to understand for someone who's new to programming.</p>
+                    </div>
+                </div>
+                <div class="info_homepage_header col-6 pb-5">
+                    <img src="img/A.png" alt="">
+                    <div>
+                        <button type="button" class="btn btn-outline-primary boton rounded-pill m-3 btn-img">
+                            <a class="btn-img text-decoration-none" href="noticies.html">Notícies</a>
+                        </button>
+                    </div>
+                    <div>
+                        <p class="m-1 grey">Kate Glotova | Art Director | 4 min read</p>
+                        <p class="m-1 mb-4 grey">September 23 ,2022</p>
+                    </div>
+                    <div class="col-sm-6 col-md-10 mb-4">
+                        <h3 class="line-height col-10">Talking now about typography!</h3>
+                    </div>
+                    <div class="col-sm-6 col-md-8">
+                        <p class="p_header">Typography in advertising often reflects a company's brand. A brand may use typography to express its theme, personality, and message.</p>
+                    </div>
+                </div>
+                <div class="col-6 pb-5">
+                    <img src="img/codi_html.png" alt="">
+                    <div>
+                        <button type="button" class="btn btn-outline-primary boton rounded-pill m-3 btn-img">
+                            <a class="btn-img text-decoration-none" href="noticies.html">Notícies</a>
+                        </button>
+                    </div>
+                <div>
+                        <p class="m-1 grey">Jessica Cordoba | Frontend Developer</p>
+                        <p class="m-1 mb-4 grey">4 min read | May 24,2022</p>
+                    </div>
+                    <div class="col-sm-6 col-md-8 mb-4" >
+                        <h3 class="line-height col-10">What you need to know about code languages</h3>
+                    </div>
+                    <div class="col-sm-6 col-md-7">
+                        <p class="p_header">Rather than having to jump into strict syntax rules, Python reads like English and is simple to understand for someone who's new to programming.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="container-fluid padding-btn border-bottom border-3">
+                <button type="button" class="btn boton m-3 btn-grande padding-btn-in">SHOW MORE</button>
+            </div>
+        </div>
+<?php get_footer() ?>
